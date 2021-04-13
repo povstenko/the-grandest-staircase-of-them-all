662751503c42835a1a42e1151b65709cae8ec4bf
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-13 12:12:14 +0300

    Recursive variant pass 3 tests with small number of bricks

--- solution.py
+++ solution.py
@@ -1,2 +1,20 @@
 def solution(n):
-    # Your code here
+    if n <= 200:
+        return str(count(1, n) - 1)
+    else:
+        return 0
+    
+    
+def count(height, left):
+    if left == 0:
+        return 1
+
+    if left < height:
+        return 0
+
+    return count(height + 1, left - height) + count(height + 1, left)
+
+print(solution(3))
+print(solution(4))
+print(solution(5))
+print(solution(200))
