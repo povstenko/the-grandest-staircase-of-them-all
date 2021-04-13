7d6d071e630ec4a1ceeefadf5cdc7bb888181b62
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-13 12:48:07 +0300

    Test 1, 3, 5, 9 failed

--- solution.py
+++ solution.py
@@ -1,18 +1,16 @@
 def solution(n):
+    def count_stairs(height, left):
+        if left == 0:
+            return 1
+
+        if left < height:
+            return 0
+
+        return count_stairs(height + 1, left - height) + count_stairs(height + 1, left)
     if n <= 200:
-        return str(count(1, n) - 1)
+        return str(count_stairs(1, n) - 1)
     else:
         return 0
-    
-    
-def count(height, left):
-    if left == 0:
-        return 1
-
-    if left < height:
-        return 0
-
-    return count(height + 1, left - height) + count(height + 1, left)
 
 print(solution(3))
 print(solution(4))
